f395e73141b7c779faa316b4ce0907273a21af1f
Christian Freitag <cfreitag.cf@googlemail.com>
2019-07-28 22:33:45 +0200

    - Fixed mapping error within boot_virt_platform_setup related to cpu peripherals - Encapsulated cpu peripheral mapping by defined check instead of comparison against 0 - Extended travis config after rebase

--- src/platform/rpi/boot/mm/virt.c
+++ src/platform/rpi/boot/mm/virt.c
@@ -29,30 +29,29 @@
  * @brief Method to setup short descriptor paging
  */
 void __bootstrap boot_virt_platform_setup( void ) {
+  // cpu local peripherals
   #if defined( BCM2709 ) || defined( BCM2710 )
-    uintptr_t gpio_peripheral_base = 0x3F000000;
-    size_t gpio_peripheral_size = 0xFFFFFF;
     uintptr_t cpu_peripheral_base = 0x40000000;
     size_t cpu_peripheral_size = 0x3FFFF;
-  #else
-    uintptr_t gpio_peripheral_base = 0x20000000;
-    size_t gpio_peripheral_size = 0xFFFFFF;
-    uintptr_t cpu_peripheral_base = 0;
-    size_t cpu_peripheral_size = 0;
-  #endif
+    uintptr_t cpu_peripheral_end = cpu_peripheral_base + cpu_peripheral_size;
 
-  uintptr_t gpio_peripheral_end = gpio_peripheral_base + gpio_peripheral_size;
-  uintptr_t cpu_peripheral_end = cpu_peripheral_base + cpu_peripheral_size;
-
-  // map cpu local peripherals if set
-  if ( 0 != cpu_peripheral_base ) {
     while ( cpu_peripheral_base < cpu_peripheral_end ) {
       // identity map gpio
-      boot_virt_map( ( uint64_t )gpio_peripheral_base, gpio_peripheral_base );
+      boot_virt_map( ( uint64_t )cpu_peripheral_base, cpu_peripheral_base );
       // next page
       cpu_peripheral_base += PAGE_SIZE;
     }
-  }
+  #endif
+
+  // GPIO related
+  #if defined( BCM2709 ) || defined( BCM2710 )
+    uintptr_t gpio_peripheral_base = 0x3F000000;
+    size_t gpio_peripheral_size = 0xFFFFFF;
+  #else
+    uintptr_t gpio_peripheral_base = 0x20000000;
+    size_t gpio_peripheral_size = 0xFFFFFF;
+  #endif
+  uintptr_t gpio_peripheral_end = gpio_peripheral_base + gpio_peripheral_size;
 
   // map gpio if set
   while ( gpio_peripheral_base < gpio_peripheral_end ) {
