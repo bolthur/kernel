04cf1b405e214a65e9c0ec7ba8eb412b93aaebd2
Christian Freitag <cfreitag.cf@googlemail.com>
2019-12-22 21:44:38 +0100

    - Changed test program compilation to specify specs file - Added a bit more test code to main

--- user/main.c
+++ user/main.c
@@ -1,13 +1,20 @@
 
 #include <stdio.h>
+#include <unistd.h>
 
 int i = 0;
 
 int main( int argc, char *argv[] ) {
+  printf( "Hello World!\r\n" );
+
   for ( int j = i + 5; i < j; i++ ) {
     char c = ( char )( ( int )'0' + i );
-    putchar( c );
+    printf( "%c", c );
   }
+
+  void *b = sbrk( 0 );
+  int *p = ( int* )b;
+  sbrk( 2 );
 
   return 0;
 }
