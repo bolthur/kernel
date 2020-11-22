5fc6fcc98318531f7a6ab868c28d6362d4f7d61e
Christian Freitag <cfreitag.cf@googlemail.com>
2020-11-22 17:19:18 +0100

    - Added temporary driver subdirectory for generic or platform specific drivers - Removed init and vfs from user folder - Renamed user folder to user libc test as it contains only test programs

--- user_libc_test/test_malloc.c
+++ user_libc_test/test_malloc.c
@@ -1,7 +1,9 @@
 
 #include <stdlib.h>
+#include <string.h>
 
 int main( int argc, char *argv[] ) {
   int* p = ( int* )malloc( sizeof( int ) * 4 );
+  memset( p, 0, sizeof( int ) * 4 );
   return 0;
 }
