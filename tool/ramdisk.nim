69faaf075cc6029aad8334f9b3915477233a49a3
Christian Freitag <cfreitag.cf@googlemail.com>
2020-11-26 00:10:50 +0100

    - Removed c code due to nim script - Added logic to build initrd with fixed name and fixed path

--- tool/ramdisk.nim
+++ tool/ramdisk.nim
@@ -1,12 +1,42 @@
+#
+# Copyright (C) 2018 - 2020 bolthur project.
+#
+# This file is part of bolthur/kernel.
+#
+# bolthur/kernel is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bolthur/kernel is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bolthur/kernel.  If not, see <http://www.gnu.org/licenses/>.
+#
 
-import os, osproc, strutils, re
+import os
+import osproc
+import strutils
+import re
 
-# FIXME: Add command line
+# create tmp dir
+createDir("./tmp")
+createDir("./tmp/boot")
 
+# FIXME: ADD DRIVER DIR AS COMMAND LINE PARAMETER
 for file in walkDirRec "../build/driver":
   let outp_shell = execProcess("file " & file)
   if contains(outp_shell, ": ELF") and contains(outp_shell, "executable"):
-    # FIXME: COPY TO TMP FOLDER
     echo file
+    let split = splitPath(file)
+    copyFile(file, "./tmp/boot/" & split.tail);
 
-# FIXME: Create tar archive
+# create tar archive
+# FIXME: MOVE NAME TO PARAMETER
+discard execProcess("tar cvf ./initrd tmp/**/* --transform='s:tmp/::g'")
+
+# remove tmp dir again
+removeDir("./tmp")
